08a23c835c3bd03aa5869f4d7a545d7166279256
daniel sanchez <daniel.sanchez21@uptc.edu.co>
2024-06-08 21:40:18 -0500

    config

---
+++ src/test/java/uptc/frw/coches/CochesApplicationTests.java
@@ -0,0 +1,13 @@
+package uptc.frw.coches;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CochesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
